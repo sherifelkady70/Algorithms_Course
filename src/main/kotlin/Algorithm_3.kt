a1202f0be5eeb4d2a5af2406a35d2ae1a56dfd1b
sherif Abdelrhman <sellkady038@gmail.com>
2024-11-30 17:12:29 +0200

    impl correlation.

--- src/main/kotlin/Algorithm_3.kt
+++ src/main/kotlin/Algorithm_3.kt
@@ -4,45 +4,41 @@ import kotlin.math.sqrt
 
 //correlation
 
-fun main(){
-    val arrX = intArrayOf(1,2,3,4,5,6)
-    val arrY = intArrayOf(1,2,3,4,5,6)
-    println(calculateCorrelation(arrX,arrY))
+fun main() {
+    val arrX = intArrayOf(1, 2, 3, 4, 5, 6)
+    val arrY = intArrayOf(1, 2, 3, 4, 5, 6)
+    println(calculateCorrelation(arrX, arrY))
 }
 
-fun calculateCorrelation(arrX : IntArray , arrY : IntArray) : Int{
-    var r =0
-    var avgX = 0
-    var avgY = 0
+fun calculateCorrelation(arrX: IntArray, arrY: IntArray) : Int {
     var topEquation = 0
-    var downEquationOne =0
+    var downEquationOne = 0
     var downEquationTwo = 0
-    var downEquation = 0
     val nOfX = arrX.size
     val nOfY = arrY.size
-    var sumX=0
-    var sumY=0
+    var sumX = 0
+    var sumY = 0
 
-    for(i in arrX){
-        sumX+=i
+    for (i in arrX) {
+        sumX += i
     }
-    avgX = sumX/nOfX
-    for(i in arrY){
-        sumY+=i
+    val avgX = sumX / nOfX
+    for (i in arrY) {
+        sumY += i
     }
-    avgY = sumY/nOfY
+    val avgY = sumY / nOfY
 
-    for(i in 0..<nOfX){
-        topEquation += (arrX[i]-avgX)*(arrY[i]-avgY)
+    for (i in 0..<nOfX) {
+        topEquation += (arrX[i] - avgX) * (arrY[i] - avgY)
     }
-    for(i in arrX){
-        downEquationOne += (i - avgX)*(i - avgX)
+    for (i in arrX) {
+        downEquationOne += (i - avgX) * (i - avgX)
     }
-    for (i in arrY){
-        downEquationTwo += (i-avgY)*(i - avgY)
+    for (i in arrY) {
+        downEquationTwo += (i - avgY) * (i - avgY)
     }
-    downEquation = sqrt(((downEquationOne*downEquationTwo).toDouble())).toInt()
-    r = topEquation/downEquation
+    val downEquation = sqrt(((downEquationOne * downEquationTwo).toDouble())).toInt()
+    val r = topEquation / downEquation
 
     return r
 }
